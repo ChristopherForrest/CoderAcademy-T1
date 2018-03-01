ba1e0dcb3767f040ae82e55b6a7a153f42e83250
ChristopherForrest <Christopherforrest92@gmail.com>
2018-03-01 16:13:21 +1100

    Fixed list

--- Terminal_App/Python_Courses.rb
+++ Terminal_App/Python_Courses.rb
@@ -1,12 +1,17 @@
+require_relative "basket"
+
 module Python_Courses
     module_function
     def courses
     rows = []
-    rows << ['Python:Beginners','Feb 19th','Feb 28th', '$20,000']
-    rows << ['Python:Intermediate','March 1st','March 31st', '$40,000' ]
-    rows << ['Python:Advanced', "April 1st", "April 30th", '$80,000']
+    rows << [1,'Python:Beginners','Feb 19th','Feb 28th', 20000]
+    rows << [2,'Python:Intermediate','March 1st','March 31st', 40000]
+    rows << [3,'Python:Advanced', "April 1st", "April 30th", 80000]
     table = Terminal::Table.new :title =>"IOC", :headings => ['Course:','Start Date:','End Date:','Price:'], :rows => rows, :style => {:width => 80}
     puts table
+    rows
+    
+    Basket.shopping_basket(rows)
     end
 
 end
--- Terminal_App/basket.rb
+++ Terminal_App/basket.rb
@@ -1,14 +1,15 @@
 require_relative "C_Sharp_Courses"
+require_relative "Python_Courses"
 require_relative "available_course"
-
+require "colorize"
 
 module Basket
     module_function
 
     def shopping_basket(rows)
-        courses_selected = []
+         
        
-    puts "Add Course | Exit"
+    puts "Add Course | Remove Course | Exit"
     
     options = ""
     options = gets.chomp
@@ -16,7 +17,12 @@ module Basket
         
         # p list[0]
         case
-        
+        when options == "r"
+            starting_balance = 0
+            IO.write("total_cost.txt",starting_balance).to_s
+            courses_selected = ["good"]
+            puts courses_selected
+            Available_Courses.options
 
             when options ==  "a"
             puts "Enter Course ID"
@@ -26,10 +32,19 @@ module Basket
 
             when
                 course_option == "1"
-                courses_selected << rows[0]
+                courses_selected = ""
+                courses_selected << IO.read("course_selections.txt")
+                courses_selected += rows[0].join(",") + "\n"
                 total = IO.read("total_cost.txt").to_i
                 total += rows[0][4]
-                IO.write("total_cost.txt",total).to_s
+                IO.write("total_cost.txt",total)
+                puts "\n"
+                puts "Your current total is: ".yellow + "$".green + IO.read("total_cost.txt").green
+                IO.write("course_selections.txt",courses_selected)
+                
+                
+                puts courses_selected
+                puts "\n"
                 Available_Courses.options
                 
                 
@@ -39,6 +54,10 @@ module Basket
                 total = IO.read("total_cost.txt").to_i
                 total += rows[1][4]
                 IO.write("total_cost.txt",total).to_s
+                puts "\n"
+                puts "Your current total is: $".yellow "$".green+ IO.read("total_cost.txt").green
+                pp courses_selected
+                puts "\n"
                 Available_Courses.options
             when
                 course_option == "3"
@@ -46,18 +65,20 @@ module Basket
                 total = IO.read("total_cost.txt").to_i
                 total += rows[2][4].to_i
                 IO.write("total_cost.txt",total).to_s
+                puts "\n"
+                puts "Your current total is: $" + IO.read("total_cost.txt")
+                pp courses_selected
+                puts "\n"
                 Available_Courses.options
+
+            
+    
                 
 
             end
             
         end
     
-        
-  
-puts IO.read("total_cost.txt")
-
-
     end
 end
 end
